ad35c9d6cada61ac76cc8ee6489234dea0c96290
TOBENTOch <info@tobento.ch>
2025-05-23 18:19:17 +0200

    added table role attributes

--- src/Renderer.php
+++ src/Renderer.php
@@ -41,6 +41,7 @@ class Renderer implements RendererInterface
         
         $attributes = new Attributes($table->getAttributes());
         $attributes->add('class', 'table');
+        $attributes->add('role', 'table');
         $html = '<div'.$attributes.'>';
         
         foreach($table->getRows() as $row) {
@@ -50,6 +51,7 @@ class Renderer implements RendererInterface
             
             $attributes = new Attributes($row->getAttributes());
             $attributes->add('class', 'table-row');
+            $attributes->set('role', 'row');
             
             if ($row->isHeading()) {
                 $attributes->add('class', 'th');
@@ -67,12 +69,18 @@ class Renderer implements RendererInterface
                     : Str::esc($column->text());
                 
                 $size = $this->getColumnSize($table->getRows(), $column->key());
+                $role = 'cell';
+                
+                if ($row->isHeading()) {
+                    $role = 'columnheader';
+                }
                 
                 if (empty($column->attributes())) {
-                    $html .= '<div class="table-col grow-'.Str::esc((string)$size).'">'.$text.'</div>';
+                    $html .= '<div class="table-col grow-'.Str::esc((string)$size).'" role="'.$role.'">'.$text.'</div>';
                 } else {
                     $attributes = new Attributes($column->attributes());
                     $attributes->add('class', 'table-col grow-'.Str::esc((string)$size));
+                    $attributes->set('role', $role);
                     $html .= '<div'.$attributes.'>'.$text.'</div>';
                 }
             }
